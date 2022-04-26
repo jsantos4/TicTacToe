52ae29685ef5e5074f91fa1092d02491e1d3532e
John Santos <johnlsantos19@gmail.com>
2022-04-26 17:32:16 -0400

    C skeleton

---
+++ attampt2.c
@@ -0,0 +1,23 @@
+unsigned int xboard, oboard = 0;
+const unsigned int spaceCodes[9] = { 2148008064, 1073774592, 536872968, 134479872, 67125316, 33555456, 8519682, 4202496, 2097686 }
+
+int checkwin() {}
+
+int checkspace(){}
+
+void turn(unsigned int space, int board) {
+	//check if valid space
+	//boards = boards | space
+}
+
+int main() {
+	int player = 1;
+	int space;
+	while (checkwin() == 0) {
+		//scanspace
+		//space = spaceCodes[space]
+		//take turn
+		player *= -1;
+	}
+	return 0;
+}
