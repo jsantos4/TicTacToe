de460ce0dd33cb2b2426b74a70406f4ff3ecd96d
jsantos4 <johnlsantos19@gmail.com>
2022-04-26 22:16:20 -0400

    Adjustments to logic, checkspace isn't quite working properly

--- src/tictactoe.c
+++ src/tictactoe.c
@@ -1,31 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 unsigned int xboard, oboard = 0;
 const unsigned int spaceCodes[9] = { 2148008064, 1073774592, 536872968, 134479872, 67125316, 33555456, 8519682, 4202496, 2097686 };
 
 int checkwin() {
-	return (xboard & (xboard << 1) & (xboard >> 1));
+	return (xboard & (xboard << 1) & (xboard >> 1) || oboard & (oboard << 1) & (oboard >> 1));
 }
 
 int checkspace(unsigned int space){
-	return space & (xboard | oboard);
+	if (space & (xboard | oboard) != 0 || space > 8){
+		printf("Invalid space, choose again\n");
+		return 1;
+	} else
+		return 0;
 }
 
-void turn(unsigned int space, int board) {
+void turn(unsigned int space, unsigned int board) {
+
 	if (board > 0)
 		xboard = xboard | space;
 	else
 		oboard = oboard | space;
+
+	printf("%u\n%u\n", xboard, oboard);
 }
 
 int main() {
+	unsigned int space;
 	int player = 1;
-	int space;
+	
 	while (checkwin() == 0) {
-		printf("SPACES:\n1 | 2 | 3\n4 | 5 | 6\n7 | 8 | 9\n");
-		scanf("%d", &space);
-		space = spaceCodes[space];
-		turn(space, player);
+		printf("SPACES:\n0 | 1 | 2\n3 | 4 | 5\n6 | 7 | 8\n");
+
+		scanf("%u", &space);
+		while (checkspace(space) != 0){
+			scanf("%d", &space);
+		}
+
+		turn(spaceCodes[space], player);
 		player *= -1;
 	}
 	return 0;
